4f22a70ea3ab1deba15378e2d78aaf2ff8d84291
Gurpreet Sidhu <gsidhu@csumb.edu>
2019-03-23 20:23:58 +0000

    Adding lab 6 files.

--- labs/lab6/dbConnection.php
+++ labs/lab6/dbConnection.php
@@ -5,6 +5,15 @@
         $username='root';
         $password='';
         
+        //checks whether the URL contains "herokuapp" (using Heroku)
+        if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+           $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+           $host = $url["host"];
+           $dbName = substr($url["path"], 1);
+           $username = $url["b199bb5d371253"];
+           $password = $url["65892484"];
+        }
+        
         //creates db connection
         $dbConn = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
         
